4fdd9330d1ee0528977db963abc3a5b89b6ff13a
MrSumada <ricksamericana.com>
2022-05-26 13:54:12 -0400

    switch from exact path to path for /saved

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -34,6 +38,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
+
+    app.use(express.static(path.join(__dirname, '../client/build')));
   })
 };
 
